aa171144666a9a7ab6861a5123377f30e5759d29
Chau-Nhi Phan <cphan2@nd.edu>
2019-03-31 10:39:26 -0400

    Can favorite or unfavorite any app in the favorites bar

--- insideND.js
+++ insideND.js
@@ -1,19 +1,41 @@
+//$(document).ready(function () {
+//    $("#UTButton").click(function () {
+//        if ($("#UTButton span").hasClass("checked")) {
+//            console.log('unfavorite');
+//            if ($("#NOVOCard").length) {
+//                console.log('NOVO exists');
+//                $("#NOVOCard").replaceWith($("#UTCard"));
+//            } else {
+//                console.log('NOVO is gone');
+//                $("#deletedList").append($("#UTCard"));
+//            }
+//            $("#UTButton span").addClass('fa-star-o').removeClass('fa-star checked');
+//        } else {
+//            console.log('favorite');
+//            $("#currentFavs").append($("#UTCard"));
+//            $("#UTButton span").addClass('fa-star checked').removeClass('fa-star-o');
+//        }
+//    });
+//});
+
 $(document).ready(function () {
-    $("#UTButton").click(function () {
-        if ($("#UTButton span").hasClass("checked")) {
+    $(".starButton").click(function () {
+        cardElement = $(this).parent().parent().parent();
+        if ($(this).find('span').hasClass("checked")) {
             console.log('unfavorite');
-            if ($("#NOVOCard").length) {
-                console.log('NOVO exists');
-                $("#NOVOCard").replaceWith($("#UTCard"));
+            console.log($("#deletedList").children().length);
+            if ($("#deletedList").children().length) {
+                console.log('replace');
+                $("#deletedList > a").replaceWith(cardElement);
             } else {
-                console.log('NOVO is gone');
-                $("#deletedList").append($("#UTCard"));
+                console.log('empty deleted list');
+                $("#deletedList").append(cardElement);
             }
-            $("#UTButton span").addClass('fa-star-o').removeClass('fa-star checked');
+            $(this).find('span').addClass('fa-star-o').removeClass('fa-star checked');
         } else {
             console.log('favorite');
-            $("#currentFavs").append($("#UTCard"));
-            $("#UTButton span").addClass('fa-star checked').removeClass('fa-star-o');
+            $("#currentFavs").append(cardElement);
+            $(this).find('span').addClass('fa-star checked').removeClass('fa-star-o');
         }
     });
 });
