dcdca579ea509be35ddd19006ceb69930c552976
Chau-Nhi Phan <cphan2@nd.edu>
2019-03-31 20:57:26 -0400

    Merged main body into framework, made home button sticky, all stars yellow

--- insideND.js
+++ insideND.js
@@ -1,28 +1,30 @@
 $(document).ready(function () {
     $(".starButton").click(function () {
         cardElement = $(this).parent().parent();
-        if ($(this).find('span').hasClass("checked")) {
+        if ($(this).find('span').hasClass('fa')) {
             console.log('unfavorite');
             if ($("#deletedList").children().length) {
                 console.log('replace');
                 cardElement.fadeOut("slow", function () {
                     $("#deletedList > .favCard").hide();
                     $("#deletedList > .favCard").replaceWith(cardElement);
+                    cardElement.find('.starButton span').removeClass('fa').addClass('far');
                     cardElement.fadeIn("slow");
                 });
             } else {
                 console.log('deleted is empty');
                 cardElement.fadeOut("slow", function () {
                     $("#deletedList").append(cardElement);
+                    cardElement.find('.starButton span').removeClass('fa').addClass('far');
                     cardElement.fadeIn("slow");
                 })
             }
-            $(this).find('span').addClass('fa-star-o').removeClass('fa-star checked');
+            $(this).find('span').addClass('far').removeClass('far');
         } else {
             console.log('favorite');
             cardElement.hide("slow", function () {
                 $("#currentFavs").append(cardElement);
-                $(this).find('.starButton span').removeClass('fa-star-o').addClass('fa-star checked');
+                cardElement.find('.starButton span').removeClass('far').addClass('fa');
                 cardElement.fadeIn("slow");
             });
         }
