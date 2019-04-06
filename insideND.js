4b3ebc6cba41bc4f3cbd81f3ae123210bac7c47c
oliviaroy20 <oliviaroy20@gmail.com>
2019-04-05 23:01:55 -0400

    create new favorites

--- insideND.js
+++ insideND.js
@@ -4,30 +4,32 @@ $(document).ready(function () {
 		console.log(cardElement);
         if ($(this).find('span').hasClass('fa')) {
             console.log('unfavorite');
-            if ($("#deletedList").children().length) {
-                console.log('replace');
-                cardElement.fadeOut("slow", function () {
-                    $("#deletedList > .favCard").hide();
-                    $("#deletedList > .favCard").replaceWith(cardElement);
-                    cardElement.find('.starButton span').removeClass('fa').addClass('far');
-                    cardElement.fadeIn("slow");
-                });
-            } else {
-                console.log('deleted is empty');
-                cardElement.fadeOut("slow", function () {
-                    $("#deletedList").append(cardElement);
-                    cardElement.find('.starButton span').removeClass('fa').addClass('far');
-                    cardElement.fadeIn("slow");
-                })
-            }
+			unfavorite(cardElement);
+            // if ($("#deletedList").children().length) {
+            //     console.log('replace');
+            //     cardElement.fadeOut("slow", function () {
+            //         $("#deletedList > .favCard").hide();
+            //         $("#deletedList > .favCard").replaceWith(cardElement);
+            //         cardElement.find('.starButton span').removeClass('fa').addClass('far');
+            //         cardElement.fadeIn("slow");
+            //     });
+            // } else {
+            //     console.log('deleted is empty');
+            //     cardElement.fadeOut("slow", function () {
+            //         $("#deletedList").append(cardElement);
+            //         cardElement.find('.starButton span').removeClass('fa').addClass('far');
+            //         cardElement.fadeIn("slow");
+            //     })
+            // }
             $(this).find('span').addClass('far').removeClass('far');
         } else {
             console.log('favorite');
-            cardElement.hide("slow", function () {
-                $("#currentFavs").append(cardElement);
-                cardElement.find('.starButton span').removeClass('far').addClass('fa');
-                cardElement.fadeIn("slow");
-            });
+            // cardElement.hide("slow", function () {
+            //     $("#currentFavs").append(cardElement);
+            //     cardElement.find('.starButton span').removeClass('far').addClass('fa');
+            //     cardElement.fadeIn("slow");
+            // });
+			favorite(cardElement);
         }
     });
     $("#body").load("index-body.html", function () {
@@ -77,40 +79,103 @@ function goToHome() {
     $("#academics").removeClass('selectedTab');
 }
 
+function unfavorite(card){
+	if ($("#deletedList").children().length) {
+		console.log('replace');
+		$(card).fadeOut("slow", function () {
+			$("#deletedList > .favCard").hide();
+			$("#deletedList > .favCard").replaceWith(card);
+			$(card).find('.starButton span').removeClass('fa').addClass('far');
+			$(card).fadeIn("slow");
+		});
+	}
+	else {
+		console.log('deleted is empty');
+		$(card).fadeOut("slow", function () {
+			$("#deletedList").append(card);
+			$(card).find('.starButton span').removeClass('fa').addClass('far');
+			$(card).fadeIn("slow");
+		})
+	}
+}
+
+function favorite(card){
+	$(card).hide("slow", function () {
+		$("#currentFavs").append(card);
+		$(card).find('.starButton span').removeClass('far').addClass('fa');
+		$(card).fadeIn("slow");
+	});
+}
+
+function addFavorite(card){
+	var img = document.createElement("img");
+	img.className = "appIcon";
+	img.src = card.children[0].children[0].src;
+	console.log(img.src);
+
+	var name = document.createElement("span");
+	name.className = "favName";
+	name.innerHTML = card.children[1].children[0].innerHTML;
+	name.setAttribute("style","height: 60px;");
+	console.log(name.innerHTML);
+
+	var favli= document.createElement("li");
+	favli.className = "favli";
+
+	favli.appendChild(img);
+	favli.appendChild(name);
+
+	var link = document.createElement("a");
+	link.href = "#";
+	link.className="favLink";
+	link.setAttribute("target", "_blank");
+	link.appendChild(favli);
+
+	var star = document.createElement("span");
+	star.className = "fa fa-star star checked";
+
+	var starButton = document.createElement("button");
+	starButton.className="starButton";
+	starButton.appendChild(star);
+
+	var liStar = document.createElement("span");
+	liStar.className = "liStar";
+	liStar.appendChild(starButton);
+
+	var fav = document.createElement("div");
+	fav.id = card.id +"Fav";
+	fav.className = "favCard";
+
+	fav.appendChild(link);
+	fav.appendChild(liStar);
+
+	favorite(fav);
+
+
+}
+
 function starClick(id){
 	var card = document.getElementById(id);
 	var star = card.children[2].children[0];
+	var fav = document.getElementById(id+"Fav");
 
 	if ($(star).hasClass('fa')) {
 		console.log("unfavorite");
 		$(star).removeClass('fa').addClass('far');
-		fav = document.getElementById(id+"Fav");
 		console.log(fav)
-		if ($("#deletedList").children().length) {
-			console.log('replace');
-			$(fav).fadeOut("slow", function () {
-				$("#deletedList > .favCard").hide();
-				$("#deletedList > .favCard").replaceWith(fav);
-				$(fav).find('.starButton span').removeClass('fa').addClass('far');
-				$(fav).fadeIn("slow");
-			});
-		}
-		else {
-			console.log('deleted is empty');
-			$(fav).fadeOut("slow", function () {
-				$("#deletedList").append(cardElement);
-				$(fav).find('.starButton span').removeClass('fa').addClass('far');
-				$(fav).fadeIn("slow");
-			})
-		}
+		unfavorite(fav);
+
 	}
 	else{
+		console.log("favoriting");
 		$(star).removeClass('far').addClass('fa');
-		$(fav).hide("slow", function () {
-			$("#currentFavs").append(fav);
-			$(fav).find('.starButton span').removeClass('far').addClass('fa');
-			$(fav).fadeIn("slow");
-		});
+		if (fav){
+			favorite(fav);
+		}
+		else{
+			addFavorite(card);
+		}
 	}
+
 
 }
